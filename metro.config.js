f9a53721b021b956555ff0ab8f19e7569e983a05
Jay <jaybhatade7@gmail.com>
2025-05-07 10:16:39 +0530

    Auth Error Fixed

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+// This is the new line you should add in, after the previous lines defaultConfig.resolver.
+
+config.resolver.unstable_enablePackageExports = false;
+
 
 module.exports = withNativeWind(config, { input: './global.css' });
